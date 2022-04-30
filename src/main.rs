a866918d55265d980724e7048c63c72f92e7c423
Sławomir Chrobak <slawomir.chrobak@gmail.com>
2022-04-30 23:53:17 +0200

    feat: basic client handler

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,24 @@
+#[macro_use]
+extern crate log;
+
 use anyhow::Result;
+use std::net::{TcpListener, TcpStream};
+
+fn handle_client(stream: TcpStream) -> Result<()> {
+    info!("Handling client: {}", stream.peer_addr()?);
+    Ok(())
+}
 
 fn main() -> Result<()> {
-    println!("Hello, world!");
+    env_logger::init();
+
+    let listener = TcpListener::bind("127.0.0.1:1935")?;
+
+    info!("RTMP Relay listening on {}", listener.local_addr()?);
+
+    for stream in listener.incoming() {
+        handle_client(stream?)?;
+    }
+
     Ok(())
 }
