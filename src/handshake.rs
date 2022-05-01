325b4c14246b630d2ea509dc9776576b3d441fcf
Sławomir Chrobak <slawomir.chrobak@gmail.com>
2022-05-01 15:37:55 +0200

    feat: implement basic rtmp handshake

---
+++ src/handshake.rs
@@ -0,0 +1,39 @@
+use anyhow::Result;
+use rml_rtmp::handshake::{Handshake, HandshakeProcessResult, PeerType};
+use std::io::prelude::*;
+use std::net::TcpStream;
+
+pub fn make_handshake(stream: &mut TcpStream) -> Result<()> {
+    let mut handshake = Handshake::new(PeerType::Server);
+    let mut buffer = [0u8; 1024];
+
+    loop {
+        let bytes_read = stream.read(&mut buffer)?;
+        let (is_completed, response_bytes) = match handshake.process_bytes(&buffer[..bytes_read])? {
+            HandshakeProcessResult::InProgress {
+                response_bytes: bytes,
+            } => {
+                info!("handshake in progress");
+                (false, bytes)
+            }
+            HandshakeProcessResult::Completed {
+                response_bytes: bytes,
+                remaining_bytes: _,
+            } => {
+                info!("handshake completed");
+                (true, bytes)
+            }
+        };
+
+        if !response_bytes.is_empty() {
+            stream.write_all(&response_bytes)?;
+        }
+
+        if is_completed {
+            stream.flush()?;
+            break;
+        }
+    }
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
+mod handshake;
+
 #[macro_use]
 extern crate log;
 
+use crate::handshake::make_handshake;
 use anyhow::Result;
+use dotenv::dotenv;
 use std::net::{TcpListener, TcpStream};
 
-fn handle_client(stream: TcpStream) -> Result<()> {
+fn handle_client(mut stream: TcpStream) -> Result<()> {
     info!("Handling client: {}", stream.peer_addr()?);
+
+    make_handshake(&mut stream)?;
+
     Ok(())
 }
 
 fn main() -> Result<()> {
+    dotenv()?;
     env_logger::init();
 
     let listener = TcpListener::bind("127.0.0.1:1935")?;
